8657f471609e8e256771daf1a541f2836abeb697
Wareus <Wareus@Monster-PC>
2015-03-22 19:20:35 +0100

    Gui started

--- Maze/src/se/hig/aod/maze/views/LabyrintView.java
+++ Maze/src/se/hig/aod/maze/views/LabyrintView.java
@@ -1,8 +1,19 @@
 package se.hig.aod.maze.views;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
 
 public class LabyrintView extends JPanel
 {
+	private static final long serialVersionUID = 1L;
 
+	public LabyrintView()
+	{
+		setSize(new Dimension(600,600));
+		setBackground(Color.blue);
+	}
+
+	
 }
