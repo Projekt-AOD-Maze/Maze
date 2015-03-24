244db1f1324e02994df806ca37ece1570e89aa15
Wareus <Wareus@Monster-PC>
2015-03-24 02:06:38 +0100

    Allmost Done!

--- Maze/src/se/hig/aod/maze/controllers/MazeController.java
+++ Maze/src/se/hig/aod/maze/controllers/MazeController.java
@@ -1,6 +1,68 @@
 package se.hig.aod.maze.controllers;
 
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import se.hig.aod.maze.generation.MazeGenerator;
+import se.hig.aod.maze.gui.MazeGUI;
+import se.hig.aod.maze.models.MazeModel;
+
 public class MazeController
 {
+	private MazeModel mazeModel;
+	private MazeGUI mazeGUI;
+	
+	public MazeController(MazeModel mazeModel, MazeGUI mazeGUI)
+	{
+		super();
+		this.mazeModel = mazeModel;
+		this.mazeGUI = mazeGUI;
+		
+		setListeners();
+		
+	}
+	
+	public void setListeners()
+	{
+		mazeGUI.setGenerateLabyrinthListener(new GenerateLabyrinthListener());
+		mazeGUI.setFindPathListener(new FindPathListener());
+		mazeGUI.setClearListener(new ClearListener());
+	}
+	
+	private class GenerateLabyrinthListener extends AbstractAction
+	{
 
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			
+			System.out.println("Generate Labyrinth");
+			
+			new Thread(new MazeGenerator(12, 12, mazeModel.getTiles())).start();
+		}
+		
+	}
+	private class FindPathListener extends AbstractAction
+	{
+		
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			System.out.println("Find Path");
+		}
+		
+	}
+	private class ClearListener extends AbstractAction
+	{
+		
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			System.out.println("Clear");
+			
+		}
+		
+	}
+	
 }
--- Maze/src/se/hig/aod/maze/views/LabyrintView.java
+++ Maze/src/se/hig/aod/maze/views/LabyrintView.java
@@ -1,18 +1,42 @@
 package se.hig.aod.maze.views;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
+
+import se.hig.aod.maze.models.MazeModel;
+import se.hig.aod.maze.models.TileModel;
+import se.hig.aod.maze.models.TileState;
 
 public class LabyrintView extends JPanel
 {
 	private static final long serialVersionUID = 1L;
 
-	public LabyrintView()
+	private MazeModel mazeModel;
+	
+	public LabyrintView(MazeModel mazeModel)
 	{
-		setSize(new Dimension(600,600));
-		setBackground(Color.blue);
+		this.mazeModel = mazeModel;
+		
+		setPreferredSize(new Dimension(600,600));
+		setBackground(Color.PINK);
+		
+		 setLayout(new GridLayout(25,25));
+		 
+		 TileModel[][] tiles = mazeModel.getTiles();
+		 
+		 for (int x = 0; x < tiles.length; x++)
+			{
+
+				for (int y = 0; y < tiles[x].length; y++)
+				{
+					add(tiles[x][y]);
+				}
+			}
 	}
 
 	
